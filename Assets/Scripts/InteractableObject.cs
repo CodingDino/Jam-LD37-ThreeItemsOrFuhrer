5fb0b959540d296ebc10ad7ac7c62ebbe3dae068
Sarah Herzog <sarah.ann.herzog@gmail.com>
2016-12-10 17:20:32 +0000

    InteractableObject added

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,77 @@
+﻿// ************************************************************************ 
+// File Name:   InteractableObject.cs 
+// Purpose:    	Runs a script based on the interact button being pressed near this item
+// Project:		Ludum Dare 37
+// Author:      Sarah Herzog  
+// Copyright: 	2016 Bounder Games
+// ************************************************************************ 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class InteractableObject : MonoBehaviour 
+{
+
+	// ********************************************************************
+	// Private Data Members 
+	// ********************************************************************
+	[SerializeField]
+	private string m_buttonInteract = "";
+	[SerializeField]
+	private bool m_useMouseClick = true;
+	[SerializeField]
+	private GameObject m_requireNearbyObject = null;
+	[SerializeField]
+	private float m_interactionRadius = 3.0f;
+	[SerializeField]
+	public UnityEvent m_onInteract = null;
+
+	// ********************************************************************
+
+	void OnMouseDown()
+	{
+		if (m_onInteract == null)
+			return;
+
+		if (m_useMouseClick)
+		{
+			TryInteract();
+		}
+	}
+
+	// ********************************************************************
+
+	void Update()
+	{
+		if (m_onInteract == null)
+			return;
+		
+		if (!m_buttonInteract.NullOrEmpty() && Input.GetKeyDown(m_buttonInteract))
+		{
+			TryInteract();
+		}
+	}
+
+	// ********************************************************************
+
+	private bool TryInteract()
+	{
+		bool objectNear = false;
+		if (m_requireNearbyObject != null)
+			objectNear = (m_requireNearbyObject.transform.position - transform.position).magnitude <= m_interactionRadius;
+
+		if (m_requireNearbyObject == null || objectNear)
+		{
+			m_onInteract.Invoke();
+			return true;
+		}
+
+		return false;
+	}
+
+	// ********************************************************************
+
+}
---
+++ Assets/Scripts/ProductDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BounderFramework;
+
+public class ProductDefinition : Archive, Data
+{
+	private string m_id;
+	public string id { get { return m_id; } }
+
+	public bool Load(JSON _JSON)
+	{
+		bool success = true;
+
+		// Required
+		success = success && _JSON["id"].Get(ref m_id);
+
+		return success;
+	}
+
+	public JSON Save()
+	{
+		JSON save = new JSON();
+
+		save["id"].data = m_id;
+
+		return save;
+	}
+}
